7421e89235c9df42367e9e2179d64dfd40c84764
aay7ush <aay7ush@gmail.com>
2024-02-23 15:06:25 +0530

    feat: setup react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Experience from './pages/Experience'
+import Hero from './pages/Hero'
+import Layout from './pages/Layout'
+import Projects from './pages/Projects'
+import Skills from './pages/Skills'
+
 const App = () => {
-  return <h1 className="text-3xl underline font-bold">Let's build</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App
