ec6dd03786d196b773037981107db2e32e0a2d82
aay7ush <aay7ush@gmail.com>
2024-02-24 16:43:02 +0530

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Experience from './pages/Experience'
 import Hero from './pages/Hero'
 import Layout from './pages/Layout'
 import Projects from './pages/Projects'
@@ -15,7 +14,6 @@ const App = () => {
           <Route index element={<Hero />} />
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
-          <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,29 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="container flex justify-between items-center">
+      <Link to="/">
+        <h1 className="text-3xl font-medium">Josef.</h1>
+      </Link>
+
+      <nav>
+        <ul className="flex gap-5 md:gap-7 items-center">
+          <li className="nav-item">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li className="contact-btn">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 export default Header
