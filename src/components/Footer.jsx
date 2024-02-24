155f79a1234e66ead889474184065a91d82f73d9
aay7ush <aay7ush@gmail.com>
2024-02-24 16:44:27 +0530

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,22 @@
+import { Github, Instagram, Twitter } from 'lucide-react'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="container flex justify-between">
+      <p>&copy; Copyright {new Date().getFullYear()}. All Rights Reserved.</p>
+
+      <div className="flex gap-5">
+        <a href="/">
+          <Github size={'1.7rem'} />
+        </a>
+        <a href="/">
+          <Twitter size={'1.7rem'} />
+        </a>
+        <a href="/">
+          <Instagram size={'1.7rem'} />
+        </a>
+      </div>
+    </footer>
+  )
 }
 export default Footer
